bf571d33954d7be944b9f9111120903437bfd694
Andrew Shchelykalnov <shchav@mail.ru>
2026-01-29 23:24:34 +0300

    Lesson02

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const title = 'Заголовок';
-let screens = 3;
+const title = 'Glo JS+REACT';
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 1700;
-let rollback = 300;
+let rollback = 30;
 let fullPrice = 2000;
-let adaptive = 57;
+let adaptive = false;
 
-alert(title);
-console.log('Привет!')
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback / 100));

e9f33b8205679143c33f89faaf6f5a567c73daf8
Shchav <shchav@mail.ru>
2026-02-02 12:09:20 +0300

    Lesson 03

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const title = 'Glo JS+REACT';
+let title = 'Glo JS+REACT';
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 1700;
 let rollback = 30;
@@ -12,3 +12,26 @@ console.log('Стоимость верстки экранов ' + screenPrice + 
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
 console.log(screens.toLowerCase().split(', '));
 console.log(fullPrice * (rollback / 100));
+
+// lesson 03
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+adaptive = prompt('Нужен ли адаптив на сайте?');
+service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(screenPrice, servicePrice1, servicePrice2);
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log(servicePercentPrice);
+if (fullPrice >= 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice > 0 && fullPrice < 15000) {
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice <= 0) {
+    console.log("Что-то пошло не так");
+}

af9c6adc7f33ac2e342902f887c70c4de43454a7
Andrew Shchelykalnov <shchav@mail.ru>
2026-03-04 16:56:39 +0300

    Lesson28

--- src/modules/helpers.js
+++
@@ -1,8 +0,0 @@
-export const debounce = (func, ms = 300) => {
-    let timer
-
-    return (...args) => {
-        clearTimeout(timer)
-        timer = setTimeout(() => { func.apply(this, args) }, ms)
-    }
-}
